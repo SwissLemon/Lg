987a882ac6bcbfa322358a5e457434372aefa160
SwissLemon <rafael_lemos1@estudante.sesisenai.org.br>
2024-10-08 21:55:44 -0300

    Fim do dia 08/10/2024

---
+++ div.js
@@ -0,0 +1,9 @@
+function div(a , b) {
+    if(b != 0){
+        return a / b
+    }else{
+        throw new error
+    }
+}
+
+module.exports = div
---
+++ sub.test.js
@@ -0,0 +1,5 @@
+const sub = require("./sub")
+
+test("Subtrair dois numeros positivos", () =>{
+    expect (sub(1,2)).toBe(1)
+})
